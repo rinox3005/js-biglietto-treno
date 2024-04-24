ed46652c6a98c6ee2c6399aa60414cd735b8b7a1
rinox3005 <peppe.rinox@gmail.com>
2024-04-24 18:37:44 +0200

    [UPD] added variables for ids in HTML and showed all the elements inside the document for the user to see

--- js/main.js
+++ js/main.js
@@ -1,16 +1,31 @@
 'use strict';
 
+// -- definisco le variabili per gli elementi con id in pagina HTML
+
+const traveldistanceOut = document.getElementById('traveldistance');
+const ageOut = document.getElementById('age');
+const pricekmOut = document.getElementById('pricekm');
+const ticketpriceOut = document.getElementById('ticketprice');
+
 // -- definisco una variabile distance e ne stabilisco il valore tramite prompt
 
 const distance = Number(prompt('Enter the distance you want to travel in kilometers (km):'));
 
 console.log('Travel distance: ', distance, 'km');
 
+// -- mostro nel documento lunghezza della tratta scelta
+
+traveldistanceOut.innerHTML = 'Travel distance: ' + distance + ' km';
+
 // -- definisco una variabile age e ne stabilisco il valore tramite prompt
 
 const age = Number(prompt('How old are you?'));
 
 console.log('Age: ', age, 'years old');
+
+// -- mostro nel documento l'etá del passeggero
+
+ageOut.innerHTML = 'Age: ' + age + ' years old';
 
 // il prezzo del biglietto è definito in base ai km (0.21 € al km)
 
@@ -19,6 +34,10 @@ console.log('Age: ', age, 'years old');
 const pricePerKm = 0.21;
 
 console.log('Current price per Km: ', pricePerKm);
+
+// -- mostro nel documento il costo a Km del biglietto
+
+pricekmOut.innerHTML = 'Current price per Km: ' + pricePerKm;
 
 // -- definisco una variabile ticketCost calcolando distance * 0.21
 
@@ -50,7 +69,13 @@ if (age < 18) {
 
     // -- mostro in console il valore con lo sconto under 18 applicato
 
-    console.log('Ticket cost: ', ticketCost, '€', '- under 18 discount applied');
+    console.log('Ticket price: ', ticketCost, '€', '- under 18 discount applied');
+
+    // -- mostro nel documento il costo del biglietto con lo sconto applicato
+
+    ticketpriceOut.innerHTML = 'Ticket price: ' + ticketCost + '€ - under 18 discount applied';
+
+    // va applicato uno sconto del 40% per gli over 65
 
 } else if (age > 65) {
 
@@ -68,11 +93,25 @@ if (age < 18) {
 
     // -- mostro in console il valore con lo sconto over 65 applicato
 
-    console.log('Ticket cost: ', ticketCost, '€', '- over 65 discount applied');
+    console.log('Ticket price: ', ticketCost, '€', '- over 65 discount applied');
+
+    // -- mostro nel documento il costo del biglietto con lo sconto applicato
+
+    ticketpriceOut.innerHTML = 'Ticket price: ' + ticketCost + '€ - over 65 discount applied';
 
 } else {
+
+    // -- se nessuna delle condizioni precedenti si é verificata calcolo il semplice costo del biglietto senza sconti
+
     ticketCost = Math.round(ticketCost * 100) / 100;
-    console.log('Ticket cost: ', ticketCost, '€');
+
+    // -- mostro in console il valore senza sconto
+
+    console.log('Ticket price: ', ticketCost, '€');
+
+    // -- mostro nel documento il costo del biglietto senza sconto applicato
+
+    ticketpriceOut.innerHTML = 'Ticket price: ' + ticketCost + '€';
 }
 
 
