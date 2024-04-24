51d769279c021287373b9780f28295d5a489306f
rinox3005 <peppe.rinox@gmail.com>
2024-04-24 18:07:10 +0200

    [UPD] added condition for over 65 discounted price with all the variables assigned

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ console.log('Age: ', age, 'years old');
 
 // -- definisco una variabile pricePerKm e le assegno il valore 0.21
 
-const pricePerKm = Number(0.21);
+const pricePerKm = 0.21;
 
 console.log('Current price per Km: ', pricePerKm);
 
@@ -24,22 +24,21 @@ console.log('Current price per Km: ', pricePerKm);
 
 let ticketCost = (distance * pricePerKm);
 
+// -- assegno un valore di scontistica alla variabile youngDiscount = 20
 
+const youngDiscount = 20;
+
+// -- assegno un valore di scontistica alla variabile oldDiscount = 40
+
+const oldDiscount = 40;
 
 
 // va applicato uno sconto del 20% per i minorenni
 
 if (age < 18) {
-
-    // -- assegno un valore di scontistica alla variabile youngDiscount = 20
-
-    const youngDiscount = Number(20);
-
     // -- definisco una variabile discountedYoung per lo sconto minorenni
 
-    const discountedYoung = Number((ticketCost * youngDiscount) / 100);
-
-    console.log(discountedYoung);
+    const discountedYoung = (ticketCost * youngDiscount) / 100;
 
     // -- definisco una variabile ticketCostYoung calcolandone il valore
 
@@ -47,31 +46,29 @@ if (age < 18) {
 
     // -- assegno alla variabile ticketCost il nuovo valore con lo sconto under 18 applicato
 
-    ticketCost = ticketCostYoung;
+    ticketCost = Math.round(ticketCostYoung * 100) / 100;
 
     // -- mostro in console il valore con lo sconto under 18 applicato
 
     console.log('Ticket cost: ', ticketCost, '€', '- under 18 discount applied');
 
+} else if (age > 65) {
+
+    // -- definisco una variabile discountedYoung per lo sconto minorenni
+
+    const discountedOld = (ticketCost * oldDiscount) / 100;
+
+    // -- definisco una variabile ticketCostOld calcolandone il valore
+
+    const ticketCostOld = (ticketCost - discountedOld);
+
+    // -- assegno alla variabile ticketCost il nuovo valore con lo sconto under 18 applicato
+
+    ticketCost = Math.round(ticketCostOld * 100) / 100;
+
+    // -- mostro in console il valore con lo sconto over 65 applicato
+
+    console.log('Ticket cost: ', ticketCost, '€', '- over 65 discount applied');
+
 }
 
-
-// } ELSE (age > 65){
-
-// va applicato uno sconto del 40% per gli over 65
-
-// -- assegno un valore di scontistica alla variabile oldDiscount = 40
-
-// -- definisco una variabile discountedOld per lo sconto minorenni ((ticketCost * oldDiscount) / 100)
-
-// -- definisco una variabile ticketCostOld calcolando il valore come (ticketCost - discountedOld)
-
-// ticketCost = ticketCostOld
-
-// }
-
-
-// L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
-
-// console.log('Ticket cost: ', ticketCost, '€');
-// console.log (ticketCost.toFixed(2)) ???
