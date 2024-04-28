a7ce02035aafc7b3da1de8b871c1ff7a56fcb4df
rinox3005 <peppe.rinox@gmail.com>
2024-04-28 12:17:52 +0200

    [UPD] displayed content and results inside the document for the user to see

--- bonus/bonus.js
+++ bonus/bonus.js
@@ -1,5 +1,13 @@
 'use strict';
 
+// -- definisco le variabili per gli elementi con id in pagina HTML
+
+const userchoice = document.getElementById('userchoice');
+const usernumber = document.getElementById('usernumber');
+const computernumber = document.getElementById('computernumber');
+const result = document.getElementById('result');
+const outcome = document.getElementById('outcome');
+
 // chiedere all'utente se vuole pari o dispari e un numero intero compreso tra 1 e 9. 
 // Generare un numero casuale compreso tra 1 e 9, che sarà la giocata del computer. 
 // Stabilire e comunicare chi ha vinto secondo le regole di pari o dispari.
@@ -8,33 +16,41 @@
 
 const userEvenOddChoice = prompt('Do you choose even or odd?');
 
-// mostro in console il contenuto di userChoice
+// mostro in console e nel documento il contenuto di userChoice
 
 console.log('The user choice for even and odd is: ', userEvenOddChoice);
+
+userchoice.innerHTML = 'The user choice for even and odd is: ' + userEvenOddChoice;
 
 // definisco una variabile con un valore di prompt 1-9 - numerico numero dell'utente (1/9) /numeroUtente
 
 const userNumberChoice = Number(prompt('Choose a number between 1 and 9'));
 
-// mostro in console il contenuto di userChoice
+// mostro in console e nel documento il contenuto di userChoice
 
 console.log('The user number of choice is: ', userNumberChoice);
+
+usernumber.innerHTML = 'The user number of choice is: ' + userNumberChoice;
 
 // definisco una variabile con un valore randomico tra 1 e 9 (valore del computer) /numeroComputer
 
 const computerNumberChoice = Math.floor((Math.random() * 9) + 1);
 
-// mostro in console il contenuto di computerNumberChoice
+// mostro in console e nel documento il contenuto di computerNumberChoice
 
 console.log('The computer number of choice is: ', computerNumberChoice);
+
+computernumber.innerHTML = 'The computer number of choice is: ' + computerNumberChoice;
 
 // definisco una variabile con la somma di numeroUtente + numeroComputer = totaleNumero
 
 const finalResult = userNumberChoice + computerNumberChoice;
 
-// mostro in console il contenuto di finalScore
+// mostro in console e nel documento il contenuto di finalResult
 
 console.log('The result is: ', finalResult);
+
+result.innerHTML = 'The result is: ' + finalResult;
 
 // definisco una variabile che ci dice se il valore totaleNumero è pari o dispari //risultatoPariDispari
 
@@ -47,10 +63,17 @@ if (finalScore === 0 && userEvenOddChoice === 'even') {
 
     console.log('Outcome: User Wins');
 
+    outcome.innerHTML = 'User Wins';
+
 } else if (finalScore !== 0 && userEvenOddChoice === 'odd') {
 
     console.log('Outcome: User Wins');
+
+    outcome.innerHTML = 'User Wins';
+
 } else {
 
     console.log('Outcome: Computer Wins');
+
+    outcome.innerHTML = 'Computer Wins';
 }
